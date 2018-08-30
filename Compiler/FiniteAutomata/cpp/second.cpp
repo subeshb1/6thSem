33d70c00da1fe40c4ff80230e17074458da9b4ea
subeshb1 <subeshb1@gmail.com>
2018-08-30 22:54:09 +0545

    Done for the Week Compiler

--- Compiler/FiniteAutomata/cpp/second.cpp
+++ Compiler/FiniteAutomata/cpp/second.cpp
@@ -38,7 +38,7 @@ void regExp(std::string regex, std::string test)
 
         std::cout << (a.test(test, getAlphabet) ? "ACCEPTED" : "REJECTED") << std::endl;
     };
-    utils::withTime(call, "METHOD 1");
+    utils::withTime(call, "THOMSON'S CONSTRUCTION");
 }
 
 void regExp2(std::string regex, std::string test)
@@ -55,34 +55,108 @@ void regExp2(std::string regex, std::string test)
                     return j;
             }
             return -1;
-        };  
+        };
         DFA a(startState.first);
 
         std::cout << (a.test(test, getAlphabet) ? "ACCEPTED" : "REJECTED") << std::endl;
     };
-    utils::withTime(call, "Method 2");
+    utils::withTime(call, "Direct Method");
+}
+
+bool testIdentifier(std::string s)
+{
+
+    auto pregex = reToPostfix("a(a|b)*");
+    auto startState = postfixToEnfa(pregex);
+    auto alphabetSet = pregex.second;
+
+    auto getAlphabet = [](int a) {
+        if (a >= 'a' && a <= 'z' || a >= 'A' && a <= 'Z' || a == '_')
+            return 0;
+        if (a >= '0' && a <= '9')
+            return 1;
+        return -1;
+    };
+    EnfaToDfa b(startState.first);
+    DFA a(b.toDfa(alphabetSet.size()));
+
+    std::cout << (a.test(s, getAlphabet) ? "ACCEPTED" : "REJECTED") << std::endl;
+}
+
+bool number(std::string s)
+{
+
+    auto pregex = reToPostfix("aa*");
+    std::cout << pregex.first;
+    auto startState = postfixToEnfa(pregex);
+    auto alphabetSet = pregex.second;
+
+    auto getAlphabet = [&](int a) {
+        if (a >= '0' && a <= '9')
+            return 0;
+        return -1;
+    };
+    EnfaToDfa b(startState.first);
+    DFA a(b.toDfa(alphabetSet.size()));
+
+    std::cout << (a.test(s, getAlphabet) ? "ACCEPTED" : "REJECTED") << std::endl;
 }
 
 int main(int argc, char const *argv[])
 {
+    // testIdentifier(argc>1?argv[1]:"functionName");
+    number(argc>1?argv[1]:"0191");
 
     if (argc > 1)
     {
-        std::ifstream file1(argv[1]);
-        std::string regex;
-        getline(file1, regex);
-        std::string test;
-        if (argc == 4)
+        if (argc == 8)
         {
-            std::ifstream file(argv[3]);
-            getline(file, test);
+            std::string type(argv[1]);
+            std::string method;
+            std::string regex;
+            std::string test;
+            for (int i = 2; i < 8; i += 2)
+            {
+                std::string arg1(argv[i]), arg2(argv[i + 1]);
+                if (arg1 == "-m")
+                {
+                    method = arg2;
+                }
+                else if (arg1 == "-r")
+                {
+
+                    if (type == "-f")
+                    {
+                        std::ifstream file1(arg2);
+                        getline(file1, regex);
+                        file1.close();
+                    }
+                    else
+                        regex = arg2;
+                }
+                else if (arg1 == "-t")
+                {
+                    if (type == "-f")
+                    {
+                        std::ifstream file(arg2);
+                        getline(file, test);
+                        file.close();
+                    }
+                    else
+                        test = arg2;
+                }
+            }
+
+            if (method == "0")
+                regExp(regex, test);
+            else
+                regExp2(regex, test);
         }
         else
         {
-            test = argv[2];
+            std::cout << "Please pass in correct arguments: \nmatch -f|-a -m <method|0,1> -r <regularEXP|file> -t <testString|file>";
         }
-        regExp2(regex, test);
     }
-    // regExp2("(a|#)bc*", "a");
+
     return 0;
 }
