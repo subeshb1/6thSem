9d3c413a73e51cd41aeb8df4937b422f9e87cba0
subeshb1 <subeshb1@gmail.com>
2018-09-25 21:45:27 +0545

    Fixed c++ const char * bug

--- Compiler/Lexer/index.js
+++ Compiler/Lexer/index.js
@@ -1727,7 +1727,7 @@ var ASM_CONSTS = [];
 
 STATIC_BASE = GLOBAL_BASE;
 
-STATICTOP = STATIC_BASE + 28640;
+STATICTOP = STATIC_BASE + 28704;
 /* global initializers */  __ATINIT__.push({ func: function() { __GLOBAL__I_000101() } }, { func: function() { __GLOBAL__sub_I_test_cpp() } }, { func: function() { __GLOBAL__sub_I_iostream_cpp() } });
 
 
@@ -1736,7 +1736,7 @@ STATICTOP = STATIC_BASE + 28640;
 
 
 
-var STATIC_BUMP = 28640;
+var STATIC_BUMP = 28704;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 
--- Compiler/Lexer/test.cpp
+++ Compiler/Lexer/test.cpp
@@ -12,23 +12,36 @@
 #include <set>
 #include "headers/re_to_postfix.h"
 #include "headers/postfix_re_to_enfa.h"
+#include <cstring>
+#include <string>
 #include <emscripten/emscripten.h>
 extern "C"
 {
-  const char *EMSCRIPTEN_KEEPALIVE callMePlease(const char *name)
+  char *EMSCRIPTEN_KEEPALIVE callMePlease(const char *name)
   {
-   
 
     Lexer a;
     fflush(stdin);
     a.setInput(name);
-    fflush(stdin);
     const std::string data = a.toString();
-    fflush(stdin);
+    char *str = new char[data.length() + 1];
+    // const
     // std::cout << data << std::endl;
-    return data.c_str();
+    std::strcpy(str, data.c_str());
+    return str;
   }
 }
+// const char *callMePlease(const char *name)
+// {
+
+//   Lexer a;
+//   fflush(stdin);
+//   a.setInput(name);
+//   fflush(stdin);
+//   const std::string data = a.toString();
+//   std::cout << data << std::endl;
+//   return data.c_str();
+// }
 int main(int argc, char const *argv[])
 {
 
@@ -37,7 +50,7 @@ int main(int argc, char const *argv[])
   // a.allTokens();
   // Lexer b("dummy.js");
   // b.toHTML();
-  callMePlease("FIRST CALL");
+  std::cout << callMePlease("FIRST asdaskldh asdn klasjdk ajslkd jalksdj lkasjd lkasjdk jakls ");
   // a.toHTML();
   return 0;
 }
